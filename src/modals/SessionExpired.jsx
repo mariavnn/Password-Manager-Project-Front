0cd8cf06753853c6fe1194c411a7726b2b667f2e
Victoria <marianieto290520@gmail.com>
2025-06-05 19:51:55 -0500

    Arreglos en el responsive del modal de session expired

--- src/modals/SessionExpired.jsx
+++ src/modals/SessionExpired.jsx
@@ -5,9 +5,9 @@ export default function SessionExpired({label, onClick}) {
 
     return (
     <div className="fixed inset-0 backdrop-blur-sm bg-opacity-80 flex items-center justify-center z-50">
-        <div class="w-[30%] h-[30%] bg-zinc-900 border-white border-1 rounded-2xl flex flex-col p-5">
+        <div class="w-[30%] h-[30%] bg-zinc-900 border-white border-1 rounded-2xl flex justify-around flex-col p-5">
             <h3 className='text-white font-bold text-xl'>Session Expired</h3>
-            <p className='h-20 text-white py-6 flex justify-center'>{label}</p>
+            <p className='text-white flex justify-center'>{label}</p>
             <div className='w-full flex justify-end'>
                 <div className='w-1/3 hover:scale-105'>
                     <PrincipalButton
